35dd17d77128662ca049014bb3e6ae4a2cfe5485
Shreya Sanap <sanapshreya2003@gmail.com>
2024-09-17 19:20:27 +0530

    same site none in login & logout

--- backend/controller/user/userSignin.js
+++ backend/controller/user/userSignin.js
@@ -31,6 +31,7 @@ async function userSignInController(req, res) {
       const tokenOption = {
         httpOnly: true,
         secure: true,
+        sameSite:'None',
       };
 
       res.cookie("token", token, tokenOption).status(200).json({
